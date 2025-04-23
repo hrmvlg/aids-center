003701ad94e288201bef66f78179e12946769353
Olya Khromova <olya.hrmvlg@gmail.com>
2025-04-23 17:25:59 +0300

    refactor: switch from BrowserRouter to HashRouter for routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 import App from './App.jsx'
 import './styles/style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={"/aids-center"}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
