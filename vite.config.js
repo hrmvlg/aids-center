93d853898e630cd504d52c5516880a44e8fe7ddd
Olya Khromova <olya.hrmvlg@gmail.com>
2025-04-23 04:29:01 +0300

    feat: update package.json and vite.config.js
    - Added vitest for testing and updated dependencies including @reduxjs/toolkit, react-redux, react-router, and vite-plugin-pages.
    - Configured Vite to use vite-plugin-pages for automatic route generation and set build output directory to 'build'.
    - Updated Vite version in devDependencies.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import Pages from 'vite-plugin-pages'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(),],
+  base: '/aids-center/',
+  build: {
+    outDir: 'build',
+  },
+
+  plugins: [
+    react(),
+    svgr(),
+    Pages({
+      dist: 'src/pages',
+      resolver: 'react',
+      moduleId: '~react-pages',
+    }),
+  ],
 })
