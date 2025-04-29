1d567dbd15f4cee260759135562662f281d409de
Olya Khromova <olya.hrmvlg@gmail.com>
2025-04-29 03:13:58 +0300

    feat: add Card component and integrate into HomePage
    - Created a new Card component for displaying information with title, subtitle, and an icon.
    - Integrated the Card component into the HomePage, displaying a list of cards with relevant data.
    - Added styles for the Card component and the HomePage layout to ensure proper display and responsiveness.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,44 @@
 import Swiper from '../components/Swiper/Swiper';
+import Card from '../components/Cards/Card';
+
+const cards = [
+    {
+        title: "Жизнь с ВИЧ",
+        subtitle: "Что делать если у меня ВИЧ?\nКак жить с ВИЧ?",
+        iconUrl: "hiv-big-icon.svg",
+        path: "life-with-aids",
+    },
+    {
+        title: "Профилактика ВИЧ",
+        subtitle: "Подробная статья как обезопасить себя и своих близких!",
+        iconUrl: "research-icon.svg",
+        path: "prevention",
+    },
+    {
+        title: "Как передаётся ВИЧ",
+        subtitle: "Мифы о передаче ВИЧ\nКак можно и нельзя заразиться ВИЧ",
+        iconUrl: "syringe-icon.svg",
+        path: "transmission",
+    }
+];
 
 export default function HomePage() {
 
     return (
-        <div className="">
+        <div className="home-page_wrapper">
             <Swiper />
+            <div className="home-page__cards">
+                {cards.map((card, index) => (
+                    <Card
+                        key={index}
+                        className="home-page__card"
+                        title={card.title}
+                        subtitle={card.subtitle}
+                        iconUrl={card.iconUrl}
+                        path={card.path}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
